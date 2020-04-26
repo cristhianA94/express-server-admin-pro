792f8f3bed6f19e54504f5a74af63a2e06dc56ab
cristhianA94 <cristhianapolo.94@gmail.om>
2020-04-25 22:09:53 -0500

    Structure pro server Express

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-// Requires
+//                       Requires
+var createError = require('http-errors');
+// Express
 var express = require('express');
+// ORM MongoDB
 var mongoose = require('mongoose');
 
+// Libs for server
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// Inicializar variables
+// Routes
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var app = express();
-
 
 // Conecct BD
 mongoose.connect('mongodb://localhost:27017/hospitalDB', { useNewUrlParser: true, useUnifiedTopology: true }, (err, res) => {
@@ -15,16 +24,42 @@ mongoose.connect('mongodb://localhost:27017/hospitalDB', { useNewUrlParser: true
 });
 
 
-// Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: "Peticion realizada correctamente"
-    });
-});
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 
-// Escuchar peticiones
-app.listen(3000, () => {
-    console.log('Express server on port \x1b[31m', '3000: \x1b[36m', 'online');
+
+
+
+
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
